28292cf509e18ec0b1540792d0cf1fc1ad8c1489
Shaygi <104856049+Shaygi@users.noreply.github.com>
2022-06-19 16:01:29 +0200

    Menüfenster fertig, Button Bilder müssen geändert werden

--- src/scenes/MenueFenster.js
+++ src/scenes/MenueFenster.js
@@ -1,6 +1,6 @@
 var config = {
     type: Phaser.AUTO,
-    width: 1500,
+    width: 1485,
     height: 700,
     physics: {
         default: 'arcade',
@@ -19,19 +19,34 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('background', 'assets/Menuebild.png');
+
+    this.load.spritesheet('backgroundanim', 'assets/Menuesprite.png', {
+        frameHeight: 1200,
+        frameWidth: 2400,
+        spacing: 0
+    });
     this.load.image('play', 'assets/Play.png');
     this.load.image('options', 'assets/Options.png');
 }
 
+var men;
 
 function create() {
-
-    menue = this.add.image(750, 360, 'background');
-    menue.setScale(0.28);
-    play = this.add.image(750, 490, 'play');
+    this.anims.create({
+        key: "anim",
+        frames: this.anims.generateFrameNumbers("backgroundanim", {
+            start: 0,
+            end: 4
+        }),
+        frameRate: 2,
+        repeat: -1
+    });
+    men = this.add.sprite(750, 350, "menueanimation");
+    men.setScale(0.6);
+    men.play("anim");
+    play = this.add.image(750, 380, 'play');
     play.setScale(0.56);
-    options = this.add.image(750, 600, 'options');
+    options = this.add.image(750, 430, 'options');
     options.setScale(0.4);
 }
 
