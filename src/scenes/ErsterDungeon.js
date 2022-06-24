dad99035612e0746f7d7258afb8f00ded11825c8
Shaygi <104856049+Shaygi@users.noreply.github.com>
2022-06-24 16:35:46 +0200

    Sprite

--- src/scenes/ErsterDungeon.js
+++ src/scenes/ErsterDungeon.js
@@ -1,12 +1,17 @@
+var cursors;
+var player;
+var wandLayer;
 class ErsterDungeon extends Phaser.Scene {
 
     constructor() {
         super('ErsterDungeon');
+
     }
 
     preload() {
         this.load.image("terrain", "assets/tilemaps/tiles.png"); //Tileset
         this.load.tilemapTiledJSON('dungeon', 'assets/tilemaps/Dungeon.json');
+        this.load.spritesheet('astro', 'assets/Astro.png', { frameWidth: 20, frameHeight: 29 });
     }
 
 
@@ -17,16 +22,102 @@ class ErsterDungeon extends Phaser.Scene {
         // layers
         let schattenLayer = dungeon.createStaticLayer("schatten", terrain, 60, 0).setScale(0.54).setDepth(-1);
         let bodenLayer = dungeon.createStaticLayer("boden", terrain, 60, 0).setScale(0.54).setDepth(-1);
-        let wandLayer = dungeon.createStaticLayer("wand", terrain, 60, 0).setScale(0.54).setDepth(-1);
+        wandLayer = dungeon.createStaticLayer("wand", terrain, 60, 0).setScale(0.54).setDepth(-1);
         let eingangLayer = dungeon.createStaticLayer("Eingang", terrain, 60, 0).setScale(0.54).setDepth(-1);
         let ausgangLayer = dungeon.createStaticLayer("Ausgang", terrain, 60, 0).setScale(0.54).setDepth(-1);
         let dekoLayer = dungeon.createStaticLayer("deko", terrain, 60, 0).setScale(0.54).setDepth(-1);
         let wasserLayer = dungeon.createStaticLayer("wasser", terrain, 60, 0).setScale(0.54).setDepth(-1);
+        player = this.physics.add.sprite(200, 450, 'astro').setScale(1.35);
+        player.setCollideWorldBounds(true);
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('astro', { start: 17, end: 23 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('astro', { start: 30, end: 33 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('astro', { start: 17, end: 23 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
+        this.anims.create({
+            key: 'turn',
+            frames: [ { key: 'astro', frame: 0 } ],
+            frameRate: 20
+        });
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('astro', { start: 10, end: 16 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'shootright',
+            frames: this.anims.generateFrameNumbers('astro', { start: 34, end: 38 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'shootleft',
+            frames: this.anims.generateFrameNumbers('astro', { start: 39, end: 43 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+
+        //  Input Events
+        cursors = this.input.keyboard.createCursorKeys();
+        this.physics.add.collider(player, wandLayer);
 
 
     }
 
-
     update() {
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-160);
+
+            player.anims.play('left', true);
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(160);
+
+            player.anims.play('right', true);
+        }
+        else if (cursors.up.isDown)
+        {
+            player.setVelocityY(-160);
+
+            player.anims.play('up', true);
+        }
+        else if (cursors.down.isDown)
+        {
+            player.setVelocityY(160);
+
+            player.anims.play('down', true);
+        }
+        else
+        {
+            player.setVelocityX(0);
+            player.setVelocityY(0);
+
+            player.anims.play('turn');
+        }
+
     }
 }
